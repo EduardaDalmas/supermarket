70d79121ef8e65a0f644c43d2065591f9ac50a9e
DalmasEduarda <eduarda.dalmas@doctorclin.com.br>
2023-02-24 15:49:25 -0300

    inicio da aplicação

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar color="cyan-3">
+        <q-toolbar-title> Supermarket Administrator </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {},
+
+  setup() {
+    return {};
+  },
+});
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,72 @@
+<template>
+  <q-page class="q-ml-xl q-mt-md">
+    <div class="row">
+      <h3>Produtos</h3>
+    </div>
+    <div class="row">
+      <div class="col-md-3 q-mr-md">
+        <q-input outlined v-model="nome" label="Nome do produto" />
+      </div>
+
+      <div class="col-md-1 q-mr-md">
+        <q-input
+          type="number"
+          outlined
+          v-model="quantidade"
+          label="Quantidade"
+        />
+      </div>
+      <div class="col-md-2">
+        <q-input
+          type="number"
+          outlined
+          v-model="preco"
+          label="Preço unitário do produto"
+        />
+      </div>
+    </div>
+    <div class="row q-mt-sm">
+      <q-checkbox
+        v-model="fidelidade"
+        label="Possuo cartão fidelidade e desejo utilizar os descontos disponíveis."
+      />
+    </div>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'IndexPage',
+
+  // se tem promoção
+  // +4 un tem 10% desconto total
+  // valor unitario maior 50 tem 5% desconto
+  // se possui fidelidade desconto de 5% no preço total (desconsidera outros descontos)
+  // produto, quantidade, preço uni, preço total, valor de desconto, valor a ser pago
+
+  setup() {
+    let nome = ref(null);
+    let quantidade = ref(null);
+    let preco = ref(null);
+    let fidelidade = ref(null);
+    let valorTotal = ref(null);
+    let valorDesconto = ref(null);
+    let valorPagar = ref(null);
+
+    // const validaDescontos = () => {};
+
+    return {
+      nome,
+      quantidade,
+      preco,
+      fidelidade,
+      valorTotal,
+      valorDesconto,
+      valorPagar,
+      // validaDescontos,
+    };
+  },
+});
+</script>
