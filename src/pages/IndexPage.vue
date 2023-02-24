18e0d8c9e176a4bac675a024e697634d612c99c5
DalmasEduarda <eduarda.dalmas@doctorclin.com.br>
2023-02-24 17:36:50 -0300

    descontos somados

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -33,15 +33,20 @@
       />
     </div>
     <div class="row q-mt-xl">
-      <q-btn color="secondary" label="Calcular" @click="somaProduto()" />
+      <q-btn
+        :disable="!nome || !preco || !quantidade"
+        color="secondary"
+        label="Calcular"
+        @click="somaProduto()"
+      />
     </div>
     <div class="q-mt-xl" v-if="verResultado">
-      <h5>Produto:{{ nome }}</h5>
-      <p><b>Quantidade:</b> {{ quantidade }}</p>
-      <p><b>Valor unitário:</b> {{ preco }}</p>
-      <p><b>Valor total:</b> {{ valorTotal }}</p>
-      <p><b>Valor desconto:</b> {{ valorDesconto }}</p>
-      <p><b>Total a pagar: </b>{{ valorPagar }}</p>
+      <h5>Produto: {{ nome }}</h5>
+      <p><b>Quantidade:</b> R$ {{ quantidade }}</p>
+      <p><b>Valor unitário:</b> R${{ preco }}</p>
+      <p><b>Valor total:</b> R$ {{ valorTotal }}</p>
+      <p><b>Valor desconto:</b> R$ {{ valorDesconto }}</p>
+      <p><b>Total a pagar: </b> R$ {{ valorPagar }}</p>
       <p>
         <i>{{ promocao }}</i>
       </p>
@@ -55,11 +60,11 @@ import { defineComponent, ref } from 'vue';
 export default defineComponent({
   name: 'IndexPage',
 
-  // se tem promoção
-  // +4 un tem 10% desconto total AQUI
-  // valor unitario maior 50 tem 5% desconto
+  // se tem promoção OK
+  // +4 un tem 10% desconto total OK
+  // valor unitario maior 50 tem 5% desconto OK
   // se possui fidelidade desconto de 5% no preço total (desconsidera outros descontos) OK
-  // produto, quantidade, preço uni, preço total, valor de desconto, valor a ser pago
+  // produto, quantidade, preço uni, preço total, valor de desconto, valor a ser pago OK
 
   setup() {
     let nome = ref('');
@@ -86,46 +91,37 @@ export default defineComponent({
         let porcentagem = valorTotal.value * (5 / 100);
         valorPagar.value = valorTotal.value - porcentagem;
         valorDesconto.value = valorTotal.value - valorPagar.value;
-        console.log(
-          'fidelidade: ',
-          valorTotal.value,
-          valorPagar.value,
-          valorDesconto.value
-        );
         promocao.value = '* Produto com desconto fidelidade aplicado.';
       } else {
         if (quantidade.value >= 4) {
           let porcentagem = valorTotal.value * (10 / 100);
           valorPagar.value = valorTotal.value - porcentagem;
           valorDesconto.value = valorTotal.value - valorPagar.value;
-          console.log(
-            'qtd maior que 4: ',
-            valorTotal.value,
-            valorPagar.value,
-            valorDesconto.value
-          );
           promocao.value =
             '* Produto com promoção de 10% na compra de 4 unidades ou mais.';
-        } else {
-          valorPagar.value = valorTotal.value;
-          valorDesconto.value = 0;
         }
 
         if (preco.value >= 50) {
           let porcentagem = valorTotal.value * (5 / 100);
           valorPagar.value = valorTotal.value - porcentagem;
           valorDesconto.value = valorTotal.value - valorPagar.value;
-          console.log(
-            'preco uni maior que 50: ',
-            valorTotal.value,
-            valorPagar.value,
-            valorDesconto.value
-          );
           promocao.value =
             '* Produto com 5% desconto na compra de produtos com preço acima de 50 reais.';
-        } else {
+        }
+
+        if (quantidade.value >= 4 && preco.value >= 50) {
+          let porcentagem = valorTotal.value * (15 / 100);
+          valorPagar.value = valorTotal.value - porcentagem;
+          valorDesconto.value = valorTotal.value - valorPagar.value;
+
+          promocao.value =
+            '* 5% desconto na compra de produtos com preço acima de 50 reais e 10% de desconto na compra de 4 unidades ou mais. ';
+        }
+
+        if (quantidade.value < 4 && preco.value < 50) {
           valorPagar.value = valorTotal.value;
           valorDesconto.value = 0;
+          promocao.value = '* Produto sem promoção disponível';
         }
       }
     };
